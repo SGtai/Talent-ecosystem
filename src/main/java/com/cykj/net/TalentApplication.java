e014810aac8d6d427d2f3d84d655988d2762bb56
86158 <981553511@qq.com>
2020-02-15 13:43:45 +0800

    人才生态圈工程

---
+++ src/main/java/com/cykj/net/TalentApplication.java
@@ -0,0 +1,15 @@
+package com.cykj.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TalentApplication.class, args);
+	}
+
+}
