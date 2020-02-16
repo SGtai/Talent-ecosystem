b03dbf159abf1e80404818555cec054c29e7bbb7
Lenovo <123148630@qq.com>
2020-02-16 15:16:15 +0800

    增加公用跳转,将layui移动到静态

---
+++ src/main/java/com/cykj/net/controller/JumpController.java
@@ -0,0 +1,18 @@
+package com.cykj.net.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jump")
+public class JumpController
+{
+
+	@RequestMapping("/{id}/{name}")
+	public String jumpfront(@PathVariable("id") String id, @PathVariable("name") String name) {
+		return "/WEB-INF/"+id+"/"+name;
+	}
+
+
+}
