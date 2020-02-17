09fadf952149903877f6743a71e7fcc6fa54b83e
Lenovo <123148630@qq.com>
2020-02-17 22:05:30 +0800

    YDL 企业注册功能完成 2020/2/17

---
+++ src/main/java/com/cykj/net/service/AdminroleService.java
@@ -0,0 +1,24 @@
+package com.cykj.net.service;
+
+import com.cykj.net.javabean.Admin;
+import com.cykj.net.javabean.Adminrole;
+import com.cykj.net.mapper.AdminRoleDao;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author 杨德禄 JX190714
+ * @create 2020-02-17 21:51
+ * @desc
+ **/
+@Service
+public class AdminroleService
+{
+	@Resource
+	private AdminRoleDao adminRoleDao;
+	public int regAdminRole(Adminrole adminrole) {
+		int sum=adminRoleDao.regAdminRole(adminrole);
+		return sum;
+	}
+}
