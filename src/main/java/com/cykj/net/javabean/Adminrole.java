6445062c8792eb4c5c021867e851447d8ea0c2a5
fanyukai <1685271417@qq.com>
2020-02-17 22:58:38 +0800

    宇恺未完成注册登陆临时上传

---
+++ src/main/java/com/cykj/net/javabean/Adminrole.java
@@ -0,0 +1,37 @@
+package com.cykj.net.javabean;
+
+
+public class Adminrole {
+
+  private long arid;
+  private String account;
+  private long roid;
+
+
+  public long getArid() {
+    return arid;
+  }
+
+  public void setArid(long arid) {
+    this.arid = arid;
+  }
+
+
+  public String getAccount() {
+    return account;
+  }
+
+  public void setAccount(String account) {
+    this.account = account;
+  }
+
+
+  public long getRoid() {
+    return roid;
+  }
+
+  public void setRoid(long roid) {
+    this.roid = roid;
+  }
+
+}
