806fc35a3f5ec7829facbad28baca77c2fe973d1
86158 <981553511@qq.com>
2020-02-29 18:08:45 +0800

    Merge branch 'master' of https://github.com/SGtai/Talent-ecosystem
    # Conflicts:
    #	src/main/java/com/cykj/net/javabean/Userlist.java

---
+++ src/main/java/com/cykj/net/javabean/Jobintension.java
@@ -0,0 +1,20 @@
+package com.cykj.net.javabean;
+
+import lombok.Data;
+
+@Data
+public class Jobintension {
+
+  private long jzyxid;
+  private long yhid;
+  private long gwid;
+  private long jlid;
+  private String workPlace;
+  private String intentionWork;
+  private String monthlyPay;
+  private String workstate;
+  private String other;
+  private String by1;
+  private String by2;
+
+}
