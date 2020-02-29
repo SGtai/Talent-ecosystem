d85edd965e2f41c8bda0889962fb95119f149ce0
86158 <981553511@qq.com>
2020-02-29 23:12:50 +0800

    Merge branch 'master' of https://github.com/SGtai/Talent-ecosystem
    # Conflicts:
    #	src/main/java/com/cykj/net/javabean/Jobintension.java

--- src/main/java/com/cykj/net/javabean/Jianli.java
+++ src/main/java/com/cykj/net/javabean/Jianli.java
@@ -26,6 +26,7 @@ public class Jianli
 	private String csTime;
 	private String yhname;
 	private String jlname;
+	private String xl;
 	private List<Jobintension> jobintension;
 	private List<Experience> experienceList;
 	private List<Undergo> undergoList;
