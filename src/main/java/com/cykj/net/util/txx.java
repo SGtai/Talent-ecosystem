58f51c28457a6ef95a5b8872c336651cb1005100
clpNB <315057742@qq.com>
2020-03-06 15:28:38 +0800

    clp MD5

---
+++ src/main/java/com/cykj/net/util/txx.java
@@ -0,0 +1,17 @@
+package com.cykj.net.util;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
+public class txx {
+
+    @Test
+    public void tt() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+
+
+        System.out.println( MD5.EncoderByMd5("admin"));
+
+    }
+}
